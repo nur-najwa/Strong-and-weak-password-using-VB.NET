7b592ae0ddc1bd3c57880f4763a3a07a7854a4ca
Najwa <najwa@iris.com.my>
2022-10-12 15:25:13 +0800

    first commit

---
+++ PasswordLength/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.lblStrongWeak = New System.Windows.Forms.Label()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(20, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Password"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(23, 48)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(234, 20)
+        Me.txtPassword.TabIndex = 1
+        Me.txtPassword.UseSystemPasswordChar = True
+        '
+        'lblStrongWeak
+        '
+        Me.lblStrongWeak.AutoSize = True
+        Me.lblStrongWeak.Location = New System.Drawing.Point(20, 71)
+        Me.lblStrongWeak.Name = "lblStrongWeak"
+        Me.lblStrongWeak.Size = New System.Drawing.Size(29, 13)
+        Me.lblStrongWeak.TabIndex = 2
+        Me.lblStrongWeak.Text = "label"
+        Me.lblStrongWeak.Visible = False
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(178, 85)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(79, 24)
+        Me.btnSubmit.TabIndex = 3
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(270, 121)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.lblStrongWeak)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Submit"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents lblStrongWeak As Label
+    Friend WithEvents btnSubmit As Button
+End Class
---
+++ PasswordLength/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub txtPassword_TextChanged(sender As Object, e As EventArgs) Handles txtPassword.TextChanged
+
+        If txtPassword.Text <> "" Then
+            If txtPassword.TextLength >= 8 Then
+                lblStrongWeak.Text = "Password is strong."
+            Else
+                lblStrongWeak.Text = "Password is weak."
+            End If
+            lblStrongWeak.Visible = True
+        Else
+            lblStrongWeak.Visible = False
+        End If
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+
+        If txtPassword.Text <> "" Then
+            If txtPassword.TextLength >= 8 Then
+                MsgBox("Your password is strong.")
+            Else
+                MsgBox("Your password is weak.")
+            End If
+        Else
+            MsgBox("Please enter password.")
+        End If
+    End Sub
+End Class
